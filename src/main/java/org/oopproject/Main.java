5fa7451099b9ad2eaeda181624204607d8c6bc2a
Ruslan Suleymanov <87182605+aqerd@users.noreply.github.com>
2024-10-03 18:02:32 +0500

    Updated Main.java

--- src/main/java/org/oopproject/Main.java
+++ src/main/java/org/oopproject/Main.java
@@ -7,12 +7,19 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 public class Main {
     public static void main(String[] args) {
         try {
+            // Здесь берём токены
             Dotenv dotenv = Dotenv.configure().directory("assets").filename("token.env").load();
-            String TOKEN = dotenv.get("TELEGRAM_BOT_TOKEN");
+            String tgBotToken = dotenv.get("TELEGRAM_BOT_TOKEN");
+            String tmdbToken = dotenv.get("TMDB_ACCESS_TOKEN");
+
+            // Создаем наш объект TG bot
             TelegramBotsLongPollingApplication bot = new TelegramBotsLongPollingApplication();
-            bot.registerBot(TOKEN, new MyBot(TOKEN));
+            bot.registerBot(tgBotToken, new MyBot(tgBotToken));
+
             System.out.println("SUCCESS: Bot is running");
+
         } catch (TelegramApiException err) {
+            // Ошибка
             err.printStackTrace();
             System.out.println("ERROR: Bot is NOT running");
         }
