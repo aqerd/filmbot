baa7409c0d75e8ce47f75c088a9436f349cdfe2e
aqerd <2195486@gmail.com>
2024-10-07 15:45:24 +0500

    Added request testing

--- src/main/java/org/oopproject/Main.java
+++ src/main/java/org/oopproject/Main.java
@@ -1,6 +1,12 @@
 package org.oopproject;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import feign.Feign;
+import feign.gson.GsonDecoder;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.oopproject.movies.ListOfResults;
+import org.oopproject.movies.SiteRequests;
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -13,8 +19,29 @@ public class Main {
 
             TelegramBotsLongPollingApplication bot = new TelegramBotsLongPollingApplication();
             bot.registerBot(tgBotToken, new MyBot(tgBotToken));
-
             System.out.println("SUCCESS: Bot is running");
+
+            SiteRequests tmdbService = Feign
+                .builder()
+                .decoder(new GsonDecoder())
+                .target(SiteRequests.class, "https://api.themoviedb.org/3");
+
+            ListOfResults popularMoviesResults = tmdbService.getPopularMovies(tmdbToken);
+
+//            System.out.println(popularMoviesResults);
+//            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//            String formattedJson = gson.toJson(popularMoviesResults);
+//            System.out.println(formattedJson);
+
+            popularMoviesResults.results.forEach(movie -> {
+                System.out.println("Object: " + movie);
+                System.out.println("Title: " + movie.title);
+                System.out.println("Overview: " + movie.overview);
+                System.out.println("Id: " + movie.id);
+                System.out.println("Rating: " + movie.rating);
+                System.out.println();
+            });
+
         } catch (TelegramApiException err) {
             err.printStackTrace();
             System.out.println("ERROR: Bot is NOT running");
