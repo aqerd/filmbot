62c7911e462bea3a8acce388467865caecf79014
aqerd <2195486@gmail.com>
2024-10-26 00:31:00 +0500

    New utils class added

---
+++ src/main/java/org/oopproject/BotUtils.java
@@ -0,0 +1,7 @@
+package org.oopproject;
+
+import java.util.Set;
+
+public class BotUtils {
+    private static final Set<String> COMMANDS = Set.of("/start", "/genre", "/year", "/help", "/setadult");
+}
