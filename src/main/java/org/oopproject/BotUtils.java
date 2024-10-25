cf46429344b77a7788a7ac1a07686e8a7f71c191
aqerd <2195486@gmail.com>
2024-10-26 00:31:30 +0500

    New method isCommand added

--- src/main/java/org/oopproject/BotUtils.java
+++ src/main/java/org/oopproject/BotUtils.java
@@ -4,4 +4,8 @@ import java.util.Set;
 
 public class BotUtils {
     private static final Set<String> COMMANDS = Set.of("/start", "/genre", "/year", "/help", "/setadult");
+
+    public static boolean isCommand(String text) {
+        return COMMANDS.contains(text);
+    }
 }
