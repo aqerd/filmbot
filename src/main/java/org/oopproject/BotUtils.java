e2d416d17bab79bdb7fd32fe22e9c3039404ad9b
aqerd <2195486@gmail.com>
2024-10-26 19:39:32 +0500

    Commands places fix in set

--- src/main/java/org/oopproject/BotUtils.java
+++ src/main/java/org/oopproject/BotUtils.java
@@ -3,7 +3,13 @@ package org.oopproject;
 import java.util.Set;
 
 public class BotUtils {
-    private static final Set<String> COMMANDS = Set.of("/start", "/genre", "/year", "/help", "/setadult", "Start", "Genre", "Year", "Help", "Set adult");
+    private static final Set<String> COMMANDS = Set.of(
+            "/start", "Start",
+            "/genre", "Genre",
+            "/year", "Year",
+            "/help", "Help",
+            "/setadult", "Set adult"
+    );
 
     public static boolean isCommand(String text) {
         return COMMANDS.contains(text);
