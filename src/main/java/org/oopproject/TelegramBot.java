606ba5bfbd01625c479b567732e29cb11f0c528c
aqerd <2195486@gmail.com>
2024-11-16 01:31:00 +0500

    Commented all lines of code ... with database

--- src/main/java/org/oopproject/TelegramBot.java
+++ src/main/java/org/oopproject/TelegramBot.java
@@ -34,7 +34,7 @@ public class TelegramBot implements LongPollingSingleThreadUpdateConsumer {
     private final TelegramClient telegramClient;
     private final ExecutorService executorService = Executors.newFixedThreadPool(4);
 //    private final Database database = new Database();
-    private final Gson gson = new Gson();
+//    private final Gson gson = new Gson();
 
     public int nOfFilms = 10;
 
