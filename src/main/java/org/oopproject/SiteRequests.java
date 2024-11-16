c66701ea947f2143a775d063135510f9420b230c
aqerd <2195486@gmail.com>
2024-11-16 19:01:01 +0500

    Updated commented code

--- src/main/java/org/oopproject/SiteRequests.java
+++ src/main/java/org/oopproject/SiteRequests.java
@@ -22,52 +22,55 @@ public interface SiteRequests {
             "&certification.lte={certification_lte}" +
             "&certification_country={certification_country}" +
             "&include_adult={include_adult}" +
-//            "&include_video={include_video}" +
             "&language={language}" +
             "&page={page}" +
-//            "&primary_release_year={primary_release_year}" +
-//            "&region={region}" +
             "&release_date.gte={release_date_gte}" +
             "&release_date.lte={release_date_lte}" +
             "&sort_by={sort_by}" +
             "&vote_average.gte={vote_average_gte}" +
             "&vote_average.gte={vote_average_lte}" +
-//            "&watch_region={watch_region}" +
-//            "&with_cast={with_cast}" +
-//            "&with_companies={with_companies}" +
-//            "&with_crew={with_crew}" +
             "&with_genres={with_genres}" +
             "&with_origin_country={with_origin_country}" +
-//            "&with_original_language={with_original_language}" +
             "&with_runtime.gte={with_runtime_gte}" +
-//            "&with_runtime.lte={with_runtime_lte}" +
-//            "&without_genres={without_genres}" +
-            "&year={year}")
+            "&year={year}"
+            // +
+            // "&include_video={include_video}" +
+            // "&primary_release_year={primary_release_year}" +
+            // "&region={region}" +
+            // "&watch_region={watch_region}" +
+            // "&with_cast={with_cast}" +
+            // "&with_companies={with_companies}" +
+            // "&with_crew={with_crew}" +
+            // "&with_original_language={with_original_language}" +
+            // "&with_runtime.lte={with_runtime_lte}" +
+            // "&without_genres={without_genres}"
+            )
     ListDeserializer findMovie(@Param("api_key") String token,
-                               @Param("certification_lte") String certificationLte,
-                               @Param("certification_country") String certificationCountry,
-                               @Param("include_adult") boolean includeAdult,
-//                           @Param("include_video") boolean includeVideo,
-                               @Param("language") String language,
-                               @Param("page") int page,
-//                           @Param("primary_release_year") int primaryReleaseYear,
-//                           @Param("region") String region,
-                               @Param("release_date_gte") String releaseDateGte,
-                               @Param("release_date_lte") String releaseDateLte,
-                               @Param("sort_by") String sortBy,
-                               @Param("vote_average_gte") float voteAverageGte,
-                               @Param("vote_average_lte") float voteAverageLte,
-//                           @Param("watch_region") String watchRegion,
-//                           @Param("with_cast") String withCast,
-//                           @Param("with_companies") String withCompanies,
-//                           @Param("with_crew") String withCrew,
-                               @Param("with_genres") String withGenres,
-                               @Param("with_origin_country") String withOriginCountry,
-//                           @Param("with_original_language") String withOriginalLanguage,
-                               @Param("with_runtime_gte") float withRuntimeGte,
-//                           @Param("with_runtime_lte") float withRuntimeLte,
-//                           @Param("without_genres") String withoutGenres,
-                               @Param("year") int year
+            @Param("certification_lte") String certificationLte,
+            @Param("certification_country") String certificationCountry,
+            @Param("include_adult") boolean includeAdult,
+            @Param("language") String language,
+            @Param("page") int page,
+            @Param("release_date_gte") String releaseDateGte,
+            @Param("release_date_lte") String releaseDateLte,
+            @Param("sort_by") String sortBy,
+            @Param("vote_average_gte") float voteAverageGte,
+            @Param("vote_average_lte") float voteAverageLte,
+            @Param("with_genres") String withGenres,
+            @Param("with_origin_country") String withOriginCountry,
+            @Param("with_runtime_gte") float withRuntimeGte,
+            @Param("year") int year
+            // ,
+            // @Param("include_video") boolean includeVideo,
+            // @Param("primary_release_year") int primaryReleaseYear,
+            // @Param("region") String region,
+            // @Param("watch_region") String watchRegion,
+            // @Param("with_cast") String withCast,
+            // @Param("with_companies") String withCompanies,
+            // @Param("with_crew") String withCrew,
+            // @Param("with_original_language") String withOriginalLanguage,
+            // @Param("with_runtime_lte") float withRuntimeLte,
+            // @Param("without_genres") String withoutGenres,
     );
 
     default ListDeserializer findMovie(MovieParameters params) {
@@ -89,8 +92,7 @@ public interface SiteRequests {
                 params.year()
         );
     }
-
-    /*
+    
     @RequestLine("GET /movie/{id}/similar?api_key={api_key}")
     ListDeserializer getSimilarMovies(@Param("api_key") String token, @Param("id") String id);
 
@@ -104,5 +106,4 @@ public interface SiteRequests {
                              @Param("language") String language,
                              @Param("page") int page,
                              @Param("year") String year);
-     */
 }
