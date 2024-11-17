11c442ea1012ee6a249c4d0497be2f249cfea30e
aqerd <2195486@gmail.com>
2024-11-17 15:53:40 +0500

    Tests from main removed

--- src/main/java/org/oopproject/Main.java
+++ src/main/java/org/oopproject/Main.java
@@ -1,21 +1,11 @@
 package org.oopproject;
 
 import feign.FeignException;
-import org.oopproject.deserializers.PersonDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
 import static org.oopproject.utils.Config.*;
-
-/****************************** PLAYGROUND ******************************/
-import static org.oopproject.utils.Validators.printPrettyJson;
-import org.oopproject.deserializers.FilmDeserializer;
-import org.oopproject.deserializers.ListDeserializer;
-import org.oopproject.deserializers.AuthDeserializer;
-import org.oopproject.parameters.MovieParameters;
-import org.oopproject.parameters.ParametersBuilder;
-/****************************** PLAYGROUND ******************************/
 
 public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
@@ -25,29 +15,6 @@ public class Main {
             TelegramBotsLongPollingApplication bot = new TelegramBotsLongPollingApplication();
             bot.registerBot(BOT_TOKEN, new TelegramBot(BOT_TOKEN));
             logger.info("Bot is running");
-
-            /****************************** PLAYGROUND ******************************/
-            // OK 200: "725201" (The Gray Man)
-            // NOT FOUND 404: "1212"
-
-            MovieParameters params = new ParametersBuilder()
-                    .withGenres("28")
-                    .withCertificationLte("PG-13")
-                    .withCertificationCountry("US")
-                    .build();
-
-            FilmDeserializer m1 = tmdbService.getMovieById(TMDB_TOKEN, 725201);
-            ListDeserializer<FilmDeserializer> m2 = tmdbService.getPopularMovies(TMDB_TOKEN);
-            AuthDeserializer m3 = tmdbService.checkAuthStatus(TMDB_TOKEN);
-            ListDeserializer<FilmDeserializer> m5 = tmdbService.getSimilarMovies(TMDB_TOKEN, 725201);
-            ListDeserializer<FilmDeserializer> m4 = tmdbService.findMovie(params);
-            ListDeserializer<FilmDeserializer> m6 = tmdbService.getRecommendationsForMovie(TMDB_TOKEN, 725201);
-            ListDeserializer<FilmDeserializer> m7 = tmdbService.searchMovie(TMDB_TOKEN, "Oppen", "en-US", 1, "2023");
-            ListDeserializer<PersonDeserializer> m8 = tmdbService.searchPerson(TMDB_TOKEN, "ryan", "en-US", 1);
-
-            printPrettyJson(m1);
-            /****************************** PLAYGROUND ******************************/
-
         } catch (TelegramApiException e) {
             e.printStackTrace();
             logger.error("Bot is NOT running. Something wrong went with Telegram API");
