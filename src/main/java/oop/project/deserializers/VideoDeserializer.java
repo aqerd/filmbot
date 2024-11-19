f6176175679f1bcfc8ccd69b5029dfcd98ac07ca
aqerd <2195486@gmail.com>
2024-11-19 23:24:15 +0500

    New request

---
+++ src/main/java/oop/project/deserializers/VideoDeserializer.java
@@ -0,0 +1,14 @@
+package oop.project.deserializers;
+
+public class VideoDeserializer {
+    public String iso_639_1; // lang
+    public String iso_3166_1; // state
+    public String name;
+    public String key;
+    public String site; // Youtube
+    public int size; // 1080p
+    public String type; // Trailer
+    public boolean official; // true
+    public String published_at;
+    public String id;
+}
